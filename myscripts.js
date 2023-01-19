5bdb38cc0898232157949a8558ccbeac4cc34722
Kalliopi <klpopi_08@hotmail.com>
2023-01-19 17:27:36 +0200

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,5 @@
 
 
-function license() {
-    document.getElementById("explainlicense").innerHTML = "Η πινακίδα θα πρέπει να περιέχει 3 γράμματα ακολουθούμενα από 4 αριθμούς,χωρίς κενό ενδιάμεσα,π.χ. IPO3245";
-    explainlicense.style.display = "block";
-};
-
-
- 
-
-
 function init(){
    const buttons =  document.querySelectorAll(".hero-section-vehicle");
    for (var i = 0; i < buttons.length; i += 1) {
@@ -28,7 +19,6 @@ function changeColor( e ){
 }
 
 window.onload = init();
-
 
 
 
