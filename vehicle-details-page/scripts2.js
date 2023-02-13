bfd6322a4606f2171c9c3337cc6742eabe38dab0
kalliopi <klpopi_08@hotmail.com>
2023-02-13 17:15:54 +0200

    adding transfer-date input in the vehicle-details form,if the vehicle is used

--- vehicle-details-page/scripts2.js
+++ vehicle-details-page/scripts2.js
@@ -2,14 +2,18 @@
 
 const yesButton = document.querySelector('#yes-button');
 const noButton = document.querySelector('#no-button');
+const transferDate = document.querySelector('.vehicle-details-transfer-date');
 
-
+//when yes buuton clicked, the tranfer-date input be displayed
 yesButton.addEventListener("click", function() {
-  yesButton.classList.toggle("active");
+  
+  yesButton.classList.add("active");
   noButton.classList.remove("active");
+  transferDate.classList.remove("hidden");
 });
 
 noButton.addEventListener("click", function() {
-  noButton.classList.toggle("active");
+  noButton.classList.add("active");
   yesButton.classList.remove("active");
+  transferDate.classList.add("hidden");
 });
